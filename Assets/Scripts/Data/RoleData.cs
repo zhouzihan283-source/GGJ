0fd7efdb5ee560f2fd0ec405b6c4f933a22a7fb6
阿航 <z871478648@163.com>
2026-01-30 22:36:02 +0800

    roleData

---
+++ Assets/Scripts/Data/RoleData.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// 角色数据
+/// </summary>
+public class RoleData
+{
+    public int RoleId;
+    public string RoleName;
+    public string State;
+    public string NormalSprite;
+    public string HighlightSprite;
+}
