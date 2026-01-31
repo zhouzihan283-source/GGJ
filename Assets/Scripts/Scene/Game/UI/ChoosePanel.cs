a53ef2c0dcab902c1f46d49e15f0e1abcce7f5ff
TheLawF <975523839@qq.com>
2026-01-31 14:47:31 +0800

    派人外出

--- Assets/Scripts/Scene/Game/UI/ChoosePanel.cs
+++ Assets/Scripts/Scene/Game/UI/ChoosePanel.cs
@@ -10,15 +10,84 @@ using UnityEngine.UI;
 public class ChoosePanel : BasePanel
 {
     public Button btCloseMask;
-
     public Button btSure;
+    
+    public Button character1;
+    public Button character2;
+    public Button character3;
+    public Button character4;
+
+    private GameObject _imageObj1;
+    private GameObject _imageObj2;
+    private GameObject _imageObj3;
+    private GameObject _imageObj4;
+    
+    private RoleObject _role1;
+    private RoleObject _role2;
+    private RoleObject _role3;
+    private RoleObject _role4;
 
 
     private void Start()
     {
+        _role1 = GameObject.Find("character1Btn").GetComponentInChildren<RoleObject>();
+        _role2 = GameObject.Find("character2Btn").GetComponentInChildren<RoleObject>();
+        _role3 = GameObject.Find("character3Btn").GetComponentInChildren<RoleObject>();
+        _role4 = GameObject.Find("character4Btn").GetComponentInChildren<RoleObject>();
+
+        _imageObj1 = GameObject.Find("character1").transform.GetChild(0).gameObject;
+        _imageObj2 = GameObject.Find("character2").transform.GetChild(0).gameObject;
+        _imageObj3 = GameObject.Find("character3").transform.GetChild(0).gameObject;
+        _imageObj4 = GameObject.Find("character4").transform.GetChild(0).gameObject;
+        
+        character1.onClick.AddListener(() =>
+        {
+            if (_imageObj1.activeInHierarchy)
+            {
+                _imageObj1.SetActive(false);
+                return;
+            }
+            _imageObj1.SetActive(true);
+        });
+        character2.onClick.AddListener(() =>
+        {
+            if (_imageObj2.activeInHierarchy)
+            {
+                _imageObj2.SetActive(false);
+                return;
+            }
+            _imageObj2.SetActive(true);
+        });
+        character3.onClick.AddListener(() =>
+        {
+            if (_imageObj3.activeInHierarchy)
+            {
+                _imageObj3.SetActive(false);
+                return;
+            }
+            _imageObj3.SetActive(true);
+        });
+        character4.onClick.AddListener(() =>
+        {
+            if (_imageObj4.activeInHierarchy)
+            {
+                _imageObj4.SetActive(false);
+                return;
+            }
+            _imageObj4.SetActive(true);
+        });
+        
         btCloseMask.onClick.AddListener(() =>
         {
             UIManager.Instance.HidePanel<ChoosePanel>();
+        });
+        
+        btSure.onClick.AddListener(() =>
+        {
+            if (_imageObj1.activeInHierarchy) _role1.GoOutSide();
+            if (_imageObj2.activeInHierarchy) _role2.GoOutSide();
+            if (_imageObj3.activeInHierarchy) _role3.GoOutSide();
+            if (_imageObj4.activeInHierarchy) _role4.GoOutSide();
         });
     }
 }
